9abf4efa79f3042b4e532d05e5df54a46567287f
Simeon Felis <simeonfelis@googlemail.com>
2012-07-15 17:47:29 +0200

    First model definition

--- music/models.py
+++ music/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    def __unicode__(self):
+        return self.artist + " - " + self.title
+
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    path_orig = models.FilePathField()
+
+class Playlist(models.Model):
+    def __unicode__(self):
+        return self.name
+
+    name = models.CharField(mox_length=255)
+    items =models.ManyToManyField(Song, through='PlaylistItem')
+
+class PlaylistItem(models.Model):
+    song = models.ForeignKey(Song)
+    playlist = models.ForeignKey(Playlist)
+    position = models.IntegerField()
