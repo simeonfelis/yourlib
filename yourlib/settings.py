989f9197b94d8cfcecbb2cc02bea064af2429d82
Simeon Felis <simeonfelis@googlemail.com>
2012-07-23 12:15:41 +0200

    README and example configs

--- yourlib/settings.py
+++ yourlib/settings.py
@@ -1,7 +1,11 @@
 #!/bin/python2
 import socket
 
+if socket.gethostname() == "example":
+    from settings_example import *
 if socket.gethostname() == "skynet":
     from settings_skynet import *
 if socket.gethostname() == "pandora":
     from settings_pandora import *
+if socket.gethostname() == "thinkpad":
+    from settings_thinkpad import *
